a9a73940754f034e4d8e6528d8fc8510121afc1f
barath <barathdhilip@gmail.com>
2023-12-08 16:16:01 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>detail</h1>
+      <h1>barath</h1>
+      <h1>73772121110</h1>
+      <h1>information technology</h1>
+      <h1>third year</h1>
+      <h1>web development</h1>
+    </div>
+  );
+}
+
+export default App;
